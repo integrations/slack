b248f9305d812d81ec67a78d4add72670c840b5a
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-10-26 22:28:06 +0200

    Pass object to Issue constructor

--- test/renderer/Issue.test.js
+++ test/renderer/Issue.test.js
@@ -5,33 +5,30 @@ const issuesClosed = require('./../fixtures/webhooks/issues.closed.json');
 
 describe('Issue rendering', () => {
   test('works for unfurls', async () => {
-    const issueMessage = new Issue(
-      issuesOpened.issue,
-      issuesOpened.repository,
-      'issues.opened',
-      true,
-    );
+    const issueMessage = new Issue({
+      issue: issuesOpened.issue,
+      repository: issuesOpened.repository,
+      unfurl: true,
+    });
     const rendered = issueMessage.getRenderedMessage();
     expect(rendered).toMatchSnapshot();
   });
 
   test('works for notification messages', async () => {
-    const issueMessage = new Issue(
-      issuesOpened.issue,
-      issuesOpened.repository,
-      'issues.opened',
-      false,
-    );
+    const issueMessage = new Issue({
+      issue: issuesOpened.issue,
+      repository: issuesOpened.repository,
+      eventType: 'issues.opened',
+    });
     const rendered = issueMessage.getRenderedMessage();
     expect(rendered).toMatchSnapshot();
   });
   test('works for minor notifications', async () => {
-    const issueMessage = new Issue(
-      issuesClosed.issue,
-      issuesClosed.repository,
-      'issues.closed',
-      false,
-    );
+    const issueMessage = new Issue({
+      issue: issuesClosed.issue,
+      repository: issuesClosed.repository,
+      eventType: 'issues.closed',
+    });
     const rendered = issueMessage.getRenderedMessage();
     expect(rendered).toMatchSnapshot();
   });
