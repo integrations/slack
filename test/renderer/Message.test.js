6df0b814c04093b38c399536d81d51c54806e3fb
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-10-27 16:53:17 +0200

    Add tests to static cleanFields method

--- test/renderer/Message.test.js
+++ test/renderer/Message.test.js
@@ -17,4 +17,66 @@ describe('Message rendering', () => {
     });
     expect(message.getBaseMessage()).toEqual({});
   });
+
+  test('works for fields', async () => {
+    const fields = [
+      {
+        title: 'Milestone',
+        value: '1',
+      },
+      {
+        title: 'Lables',
+        value: 'bug, wip',
+      },
+    ];
+    expect(Message.cleanFields(fields, 1)).toEqual([
+      {
+        title: 'Milestone',
+        value: '1',
+        short: true,
+      },
+    ]);
+  });
+
+  test('works for empty fields', async () => {
+    const fields = [
+      {
+        title: 'Milestone',
+        value: null,
+      },
+      {
+        title: 'Lables',
+        value: 'bug, wip',
+      },
+    ];
+    expect(Message.cleanFields(fields, 1, false)).toEqual([
+      {
+        title: 'Lables',
+        value: 'bug, wip',
+      },
+    ]);
+  });
+
+  test('works for fields without the short key', async () => {
+    const fields = [
+      {
+        title: 'Milestone',
+        value: 'very large milestone name',
+      },
+      {
+        title: 'Lables',
+        value: 'bug, wip',
+      },
+    ];
+    expect(Message.cleanFields(fields, Infinity, false)).toEqual([
+      {
+        title: 'Milestone',
+        value: 'very large milestone name',
+      },
+      {
+        title: 'Lables',
+        value: 'bug, wip',
+      },
+    ]);
+  });
 });
