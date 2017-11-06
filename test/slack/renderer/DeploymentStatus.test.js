e9ac9181c32303ba88e9e75fd44120412c7c5b1f
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-11-06 15:57:32 +0000

    Implement deployment statuses e2e

--- test/slack/renderer/DeploymentStatus.test.js
+++ test/slack/renderer/DeploymentStatus.test.js
@@ -3,21 +3,57 @@ const deploymentStatusPendingFixture = require('../../fixtures/webhooks/deployme
 const deploymentStatusSuccessFixture = require('../../fixtures/webhooks/deployment/status_success.json');
 
 describe('Deployment status rendering', () => {
-  test('works for pending status', async () => {
-    const deploymentStatusMessage = new DeploymentStatus({
-      deploymentStatus: deploymentStatusPendingFixture.deployment_status,
-      deployment: deploymentStatusPendingFixture.deployment,
-      repository: deploymentStatusPendingFixture.repository,
+  describe('(attachment)', () => {
+    test('works for pending status', () => {
+      const deploymentStatusMessage = new DeploymentStatus({
+        deploymentStatus: deploymentStatusPendingFixture.deployment_status,
+        deployment: deploymentStatusPendingFixture.deployment,
+        repository: deploymentStatusPendingFixture.repository,
+      });
+      expect(deploymentStatusMessage.getAttachment()).toMatchSnapshot();
     });
-    expect(deploymentStatusMessage.getRenderedMessage()).toMatchSnapshot();
-  });
 
-  test('works for success status', async () => {
-    const deploymentStatusMessage = new DeploymentStatus({
-      deploymentStatus: deploymentStatusSuccessFixture.deployment_status,
-      deployment: deploymentStatusSuccessFixture.deployment,
-      repository: deploymentStatusSuccessFixture.repository,
+    test('works for success status', () => {
+      const deploymentStatusMessage = new DeploymentStatus({
+        deploymentStatus: deploymentStatusSuccessFixture.deployment_status,
+        deployment: deploymentStatusSuccessFixture.deployment,
+        repository: deploymentStatusSuccessFixture.repository,
+      });
+      expect(deploymentStatusMessage.getAttachment()).toMatchSnapshot();
     });
-    expect(deploymentStatusMessage.getRenderedMessage()).toMatchSnapshot();
+    test('works for failure status', () => {
+      const deploymentStatus = {
+        ...deploymentStatusSuccessFixture.deployment_status,
+        state: 'failure',
+      };
+      const deploymentStatusMessage = new DeploymentStatus({
+        deploymentStatus,
+        deployment: deploymentStatusSuccessFixture.deployment,
+        repository: deploymentStatusSuccessFixture.repository,
+      });
+      expect(deploymentStatusMessage.getAttachment()).toMatchSnapshot();
+    });
+    test('works for error status', () => {
+      const deploymentStatus = {
+        ...deploymentStatusSuccessFixture.deployment_status,
+        state: 'error',
+      };
+      const deploymentStatusMessage = new DeploymentStatus({
+        deploymentStatus,
+        deployment: deploymentStatusSuccessFixture.deployment,
+        repository: deploymentStatusSuccessFixture.repository,
+      });
+      expect(deploymentStatusMessage.getAttachment()).toMatchSnapshot();
+    });
+  });
+  describe('(full message)', () => {
+    test('works', () => {
+      const deploymentStatusMessage = new DeploymentStatus({
+        deploymentStatus: deploymentStatusPendingFixture.deployment_status,
+        deployment: deploymentStatusPendingFixture.deployment,
+        repository: deploymentStatusPendingFixture.repository,
+      });
+      expect(deploymentStatusMessage.getRenderedMessage()).toMatchSnapshot();
+    });
   });
 });
