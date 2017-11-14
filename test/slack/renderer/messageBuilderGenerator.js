d37a0455ba962293fea3387e2e1520ca0c1c83b3
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-11-14 12:42:07 +0000

    Make puppeteer execusion synchronous; update images

--- test/slack/renderer/messageBuilderGenerator.js
+++ test/slack/renderer/messageBuilderGenerator.js
@@ -10,10 +10,7 @@ const RJSON = require('relaxed-json');
 
 process.setMaxListeners(0);
 
-async function getMessageBuilderImage(message, localPath) {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  page.setViewport({ width: 1000, height: 600, deviceScaleFactor: 2 });
+async function getMessageBuilderImage(page, message, localPath) {
   await page.goto(`https://api.slack.com/docs/messages/builder?msg=${encodeURIComponent(message)}`);
   await page.waitForNavigation({ waitUntil: 'networkidle' });
   await page.waitForSelector('#message_loading_indicator', { hidden: true, timeout: 120000 });
@@ -38,39 +35,56 @@ async function getMessageBuilderImage(message, localPath) {
   }
 
   await screenshotDOMElement('#msgs_div');
+}
 
+async function main() {
+  const blackList = ['AbstractIssue', 'Message'];
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1000, height: 600, deviceScaleFactor: 2 });
+  let snapshotsByFile = {}
+  fs.readdirSync(path.join(__dirname, '__snapshots__')).forEach(async (file) => {
+    // eslint-disable-next-line no-useless-escape
+    const match = named(new RegExp('^(:<class>[A-Za-z]+).test\.js\.snap')).exec(file);
+    if (match) {
+      if (blackList.indexOf(match.captures.class[0]) > -1) {
+        // if snapshot is on black list, don't process any further
+        return;
+      }
+      const folderName = `__snapshots__/${match.captures.class[0]}`;
+      if (!fs.existsSync(path.join(__dirname, folderName))) {
+        fs.mkdirSync(path.join(__dirname, folderName));
+      }
+
+      const snapshots = require(path.join(__dirname, `__snapshots__/${file}`));
+      Object.keys(snapshots).forEach((snapshot) => {
+        const cleaned = snapshots[snapshot]
+          .replace(/Object /g, '')
+          .replace(/Array /g, '')
+          .replace(/\n/g, '');
+        let message = JSON.parse(RJSON.transform(cleaned));
+        if (!message.attachments) {
+          message = { attachments: [message] };
+        }
+        snapshotsByFile[`${folderName}/${snapshot}.png`] = message;
+        // below code is to generate examples.md. To be re-integrated somewhere else
+        // const dataToAppend = `<a href="https://api.slack.com/docs/messages/builder?msg=${encodeURIComponent(JSON.stringify(message))}">${snapshot}</a>\n\n`;
+        // fs.appendFile(path.join(__dirname, '../../../lib/slack/renderer/examples.md'), dataToAppend, (err) => {
+        //   if (err) throw err;
+        // });
+      });
+    }
+  });
+  /* eslint-disable */
+  for (snapshot of Object.keys(snapshotsByFile)) {
+    await getMessageBuilderImage(
+      page,
+      JSON.stringify(snapshotsByFile[snapshot]),
+      path.join(__dirname, snapshot)
+    );
+  }
+  /* eslint-enable */
   await browser.close();
 }
 
-const blackList = ['AbstractIssue', 'Message'];
-fs.readdirSync(path.join(__dirname, '__snapshots__')).forEach((file) => {
-  // eslint-disable-next-line no-useless-escape
-  const match = named(new RegExp('^(:<class>[A-Za-z]+).test\.js\.snap')).exec(file);
-  if (match) {
-    if (blackList.indexOf(match.captures.class[0]) > -1) {
-      // if snapshot is on black list, don't process any further
-      return;
-    }
-    const folderName = `__snapshots__/${match.captures.class[0]}`;
-    if (!fs.existsSync(path.join(__dirname, folderName))) {
-      fs.mkdirSync(path.join(__dirname, folderName));
-    }
-
-    const snapshots = require(path.join(__dirname, `__snapshots__/${file}`));
-    Object.keys(snapshots).forEach(async (snapshot) => {
-      const cleaned = snapshots[snapshot]
-        .replace(/Object /g, '')
-        .replace(/Array /g, '')
-        .replace(/\n/g, '');
-      let message = JSON.parse(RJSON.transform(cleaned));
-      if (!message.attachments) {
-        message = { attachments: [message] };
-      }
-      const dataToAppend = `<a href="https://api.slack.com/docs/messages/builder?msg=${encodeURIComponent(JSON.stringify(message))}">${snapshot}</a>\n\n`;
-      fs.appendFile(path.join(__dirname, '../../../lib/slack/renderer/examples.md'), dataToAppend, (err) => {
-        if (err) throw err;
-      });
-      // await getMessageBuilderImage(JSON.stringify(message), path.join(__dirname, `${folderName}/${snapshot}.png`));
-    });
-  }
-});
+main();
