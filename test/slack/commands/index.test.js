aee2565b6e5997bc9bc1e900b6aa8520b6c04823
Brandon Keepers <bkeepers@github.com>
2017-11-16 09:16:41 -0500

    Use nock.back to cache response

--- test/slack/commands/index.test.js
+++ test/slack/commands/index.test.js
@@ -2,6 +2,8 @@ process.env.SLACK_VERIFICATION_TOKEN = 'test';
 
 const request = require('supertest');
 const createProbot = require('probot');
+const nock = require('nock');
+
 const slack = require('../../../lib/slack');
 const command = require('../../fixtures/slack/command.subscribe');
 
@@ -14,12 +16,14 @@ describe('commands', () => {
   });
 
   describe('/github subscribe https://github.com/owner/repo', () => {
-    test('status 200', () => {
-      const req = request(probot.server).post('/slack/command').send(command);
+    test('status 200', (done) => {
+      nock.back('repo/atom.json', (nockDone) => {
+        const req = request(probot.server).post('/slack/command').send(command);
 
-      return req.expect(200, {
-        response_type: 'in_channel',
-        text: 'Subscribed <#C2147483705> to <https://github.com/atom/atom|atom/atom>',
+        req.expect(200, {
+          response_type: 'in_channel',
+          text: 'Subscribed <#C2147483705> to <https://github.com/atom/atom|atom/atom>',
+        }).then(nockDone).then(done);
       });
     });
   });
