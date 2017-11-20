70b076640f41d01f82aa75ce0bf1aedc5e57858e
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-11-20 09:38:40 +0000

    Trying to make it work

--- test/slack/index.test.js
+++ test/slack/index.test.js
@@ -5,6 +5,7 @@ const request = require('supertest');
 
 const slack = require('../../lib/slack');
 const linkSharedOne = require('../fixtures/unfurls/links_shared_one.json');
+const repoFixture = require('../fixtures/repo');
 
 describe('end to end unfurls', () => {
   let probot;
@@ -13,19 +14,25 @@ describe('end to end unfurls', () => {
     probot = createRobot({});
     probot.load(slack);
   });
-  describe('https://github.com/bkeepers/dotenv', () => {
-    test('works', () => {
-      nock.back('repo/bkeepers-dotenv.json', (nockDone) => {
-        const req = request(probot.server)
-          .post('/slack/events')
-          .send(linkSharedOne);
+  test('https://github.com/bkeepers/dotenv', () => {
+    const req = request(probot.server)
+      .post('/slack/events')
+      .send(linkSharedOne);
 
-        // unfurl in Slack
-        nock('https://slack.com')
-          .post('/api/chat.unfurl');
+    const scope1 = nock('https://api.github.com').get('/repos/bkeepers/dotenv')
+     .reply(200, repoFixture);
 
-        req.expect(200).then(nockDone);
-      });
-    });
+    // unfurl in Slack
+    // const scope = nock('https://slack.com')
+    //   .log(console.log)
+    //   .post('/api/chat.unfurl')
+    //   .reply(200, {});
+
+    req.expect(200);
+    if (!scope1.isDone()) {
+      console.error('pending mocks: %j', scope1.pendingMocks());
+    }
+    expect(scope1.isDone()).toBe(true);
+    // expect(scope.isDone()).toBe(true);
   });
 });
