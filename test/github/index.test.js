8701c38ea4eefa5fdec1821fa85577e2b8d8b465
Brandon Keepers <bkeepers@github.com>
2017-11-21 12:39:28 -0600

    Get test passing

--- test/github/index.test.js
+++ test/github/index.test.js
@@ -1,19 +1,21 @@
 process.env.SLACK_VERIFICATION_TOKEN = 'shutup';
 
 const createProbot = require('probot');
+const GitHub = require('github');
 
 const app = require('../../lib/github');
 
 const installationCreated = require('../fixtures/webhooks/installation.created.json');
-const installationDeleted = require('../fixtures/webhooks/installation.deleted.json');
+// const installationDeleted = require('../fixtures/webhooks/installation.deleted.json');
 
 describe('github', () => {
   describe('installations', () => {
     test('installation created records', async () => {
-      const probot = createProbot({});
-      const robot = probot.load(app)
-
-      console.log("WTF?", robot.models);
+      const probot = createProbot({
+        cert: process.env.PRIVATE_KEY,
+      });
+      const robot = probot.load(app);
+      robot.auth = jest.fn().mockReturnValue(Promise.resolve(new GitHub()));
 
       await probot.receive({
         event: 'installation',
@@ -21,9 +23,9 @@ describe('github', () => {
       });
 
       // expect that records exists
-      const installation = await robot.models.Installation.findOne({where: {
-        githubId: installationCreated.installation.id
-      }});
+      const installation = await robot.models.Installation.findOne({
+        where: { githubId: installationCreated.installation.id } });
+
       expect(installation).toBeTruthy();
     });
   });
