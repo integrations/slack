7ac302113ef2715b9cbc789fef8a949393b9a74a
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-11-28 20:48:59 +0000

    Make all the tests pass

--- test/integration/subscriptions.test.js
+++ test/integration/subscriptions.test.js
@@ -15,7 +15,7 @@ describe('Integration: subscriptions', () => {
 
   // todo: failing to install slack app
 
-  describe('unathenticated user', () => {
+  describe('unauthenticated user', () => {
     test('is prompted to authenticate before subscribing', async () => {
       const { probot } = helper;
 
@@ -29,7 +29,7 @@ describe('Integration: subscriptions', () => {
 
       // User types slash command
       const command = fixtures.slack.command({
-        text: 'subscribe https://github.com/atom/atom',
+        text: 'subscribe https://github.com/kubernetes/kubernetes',
       });
       const req = request(probot.server).post('/slack/command').send(command);
       const res = await req.expect(200);
@@ -59,7 +59,8 @@ describe('Integration: subscriptions', () => {
       }).reply(200);
 
       await request(probot.server).get('/github/oauth/callback').query({ state })
-        .expect(302).expect('Location',
+        .expect(302)
+        .expect('Location',
           `slack://channel?team=${command.team_id}&channel=${command.channel_id}`,
         );
 
@@ -67,44 +68,67 @@ describe('Integration: subscriptions', () => {
     });
   });
 
-  // unauthenticated user
-    // subscribe to repo they have access to
-    // subscribe to repo they don't have access to
-
-  // authenticaed
-    // app is not installed
-
-  test('successfully subscribing to a repository', async () => {
-    const { probot } = helper;
-
-    const requests = {
-      account: nock('https://api.github.com').get('/orgs/atom').reply(200, fixtures.org),
-      repo: nock('https://api.github.com').get('/repos/atom/atom').reply(200, fixtures.repo),
-    };
-
-    const command = fixtures.slack.command({
-      text: 'subscribe https://github.com/atom/atom',
+  describe('authenticated user', () => {
+    beforeEach(async () => {
+      // create user
+      const slackWorkspace = await helper.robot.models.SlackWorkspace.create({
+        slackId: 'T0001',
+        accessToken: 'xoxp-token',
+      });
+      const user = await helper.robot.models.User.create();
+      await helper.robot.models.SlackUser.create({
+        slackId: 'U2147483697',
+        userId: user.dataValues.id,
+        slackWorkspaceId: slackWorkspace.dataValues.id,
+      });
+      await helper.robot.models.GitHubUser.create({
+        githubId: 2,
+        userId: user.dataValues.id,
+        accessToken: 'github-token',
+      });
     });
 
-    const req = request(probot.server).post('/slack/command').send(command);
+    test('successfully subscribing to a repository', async () => {
+      const { probot } = helper;
 
-    await req.expect(200).expect((res) => {
-      expect(res.body).toMatchSnapshot();
+      const requests = {
+        account: nock('https://api.github.com').get('/orgs/kubernetes').reply(200, fixtures.org),
+        repo: nock('https://api.github.com').get('/repos/kubernetes/kubernetes').reply(200, fixtures.repo),
+        userInstallationRespositories: nock('https://api.github.com').get('/user/installations/1/repositories').reply(
+          200, {
+            repositories: [{
+              id: fixtures.repo.id,
+            }],
+          },
+        ),
+      };
+
+      const command = fixtures.slack.command({
+        text: 'subscribe https://github.com/kubernetes/kubernetes',
+      });
+
+      const req = request(probot.server).post('/slack/command').send(command);
+
+      await req.expect(200).expect((res) => {
+        expect(res.body).toMatchSnapshot();
+      });
+
+      expect(requests.account.isDone()).toBe(true);
+      expect(requests.repo.isDone()).toBe(true);
     });
 
-    expect(requests.account.isDone()).toBe(true);
-    expect(requests.repo.isDone()).toBe(true);
-  });
+    test('subscribing with a bad url', async () => {
+      const { probot } = helper;
 
-  test('subsscribing with a bad url', async () => {
-    const { probot } = helper;
+      const command = fixtures.slack.command({
+        text: 'subscribe wat?',
+      });
 
-    const command = fixtures.slack.command({ text: 'subscribe wat?' });
+      const req = request(probot.server).post('/slack/command').send(command);
 
-    const req = request(probot.server).post('/slack/command').send(command);
-
-    await req.expect(200).expect((res) => {
-      expect(res.body).toMatchSnapshot();
+      await req.expect(200).expect((res) => {
+        expect(res.body).toMatchSnapshot();
+      });
     });
   });
 });
