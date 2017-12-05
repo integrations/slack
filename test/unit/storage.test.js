3d1547ef50527c0c7770f3f81087ea68be13884e
Brandon Keepers <bkeepers@github.com>
2017-12-05 16:13:38 -0600

    Avoid raising an error if cache not found

---
+++ test/unit/storage.test.js
@@ -0,0 +1,14 @@
+const storage = require('../../lib/storage');
+
+describe('storage', () => {
+  beforeEach(() => storage.clear());
+
+  test('sets and gets value', async () => {
+    await storage.set('foo', 'bar');
+    expect(await storage.get('foo')).toEqual('bar');
+  });
+
+  test('returns null for unset keys', async () => {
+    expect(await storage.get('nope')).toBe(null);
+  });
+});
