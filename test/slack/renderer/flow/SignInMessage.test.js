7680a3a61ef11cf93e39eaba0ca0546aaaccc0f6
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-12-07 23:21:06 +0000

    Uppercase l

--- test/slack/renderer/flow/SignInMessage.test.js
+++ test/slack/renderer/flow/SignInMessage.test.js
@@ -3,7 +3,7 @@ const { SignInMessage } = require('../../../../lib/slack/renderer/flow');
 describe('SignInMessage rendering', () => {
   test('works', async () => {
     const message = new SignInMessage({
-      signInlink: 'https://example.org/github/oauth/login?state=abcdef',
+      signInLink: 'https://example.org/github/oauth/login?state=abcdef',
     });
     expect(message.getRenderedMessage()).toMatchSnapshot();
   });
