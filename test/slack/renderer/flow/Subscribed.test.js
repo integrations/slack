e7defa27aec1613bfea2b8fa2ca2803608b8cc41
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-12-11 19:01:45 +0000

    Remove commented code

--- test/slack/renderer/flow/Subscribed.test.js
+++ test/slack/renderer/flow/Subscribed.test.js
@@ -2,16 +2,6 @@ const { Subscribed } = require('../../../../lib/slack/renderer/flow');
 const repoFixture = require('../../../fixtures/repo.json');
 
 describe('Subscribed message rendering', () => {
-//   test('works for error: no-installation', async () => {
-//     const message = new SubscribeFlowMessage({
-//       error: {
-//         type: 'no-installation',
-//         url: 'https://github.com/apps/url-to-slack-app',
-//       },
-//     });
-//     expect(message.getRenderedMessage()).toMatchSnapshot();
-//   });
-
   test('works for successfully subscribed', async () => {
     const message = new Subscribed({
       channelId: 'C1234',
