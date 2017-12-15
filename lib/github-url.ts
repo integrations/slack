499f89ee0f9e80487245e4c977863ada0f39d6ea
Wilhelm Klopp <wilhelm.klopp@github.com>
2017-12-15 11:59:02 +0000

    Fix tslint errors

--- lib/github-url.ts
+++ lib/github-url.ts
@@ -1,36 +1,37 @@
 // FIXME: replace this with a legit npm module, preferrably one that already exists
 /* eslint-disable no-useless-escape, guard-for-in, no-restricted-syntax */
+/* tslint:disable:forin */
 
-const { named } = require('named-regexp');
+import { named } from "named-regexp";
 
-const base = '(?:https:\/\/github.com\/)?';
-const owner = '(:<owner>[^\/]+)';
-const repo = '(:<repo>[^\/]+)';
+const base = "(?:https:\/\/github.com\/)?";
+const owner = "(:<owner>[^\/]+)";
+const repo = "(:<repo>[^\/]+)";
 const nwo = `${base}${owner}\/${repo}`;
 
-const line = 'L(:<line>\\d+)';
+const line = "L(:<line>\\d+)";
 
-interface routes {
+interface IRoutes {
   [key: string]: string;
 }
-const routes: routes = {
-  blob: `^${nwo}\/blob/(:<ref>[^\/]+)\/(:<path>.+?)(?:#${line}(?:-${line})?)?$`, // (?:#L(:<start>\\d+)(?-(:<end>\\d+))?)?
+const routes: IRoutes = {
+  account: `^${base}${owner}$`,
+  blob: `^${nwo}\/blob/(:<ref>[^\/]+)\/(:<path>.+?)(?:#${line}(?:-${line})?)?$`,
   comment: `^${nwo}\/(?:issues|pull)\/(:<number>\\d+)#issuecomment-(:<id>\\d+)`,
   issue: `^${nwo}\/issues\/(:<number>\\d+)$`,
   pull: `^${nwo}\/pull\/(:<number>\\d+)$`,
   repo: `^${nwo}$`,
-  account: `^${base}${owner}$`,
 };
 
 module.exports = (url: string) => {
   for (const type in routes) {
     const match = named(new RegExp(routes[type])).exec(url);
     if (match) {
-      interface result {
-        type: string,
-        [key: string]: string,
+      interface IResult {
+        type: string;
+        [key: string]: string;
       }
-      const result: result = {
+      const result: IResult = {
         type,
       };
       Object.keys(match.captures).forEach((name) => {
