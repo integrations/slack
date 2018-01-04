b80360d4132f0b68be45695d3484b3d15aafc850
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-01-04 11:33:50 -0600

    Add deployment_status integration test

--- test/integration/notifications.test.js
+++ test/integration/notifications.test.js
@@ -8,6 +8,7 @@ const { probot } = helper;
 const issuePayload = require('../fixtures/webhooks/issues.opened');
 const pullRequestPayload = require('../fixtures/webhooks/pull_request.opened');
 const publicEventPayload = require('../fixtures/webhooks/public');
+const deploymentStatusPayload = require('../fixtures/webhooks/deployment/status_success');
 
 describe('Integration: notifications', () => {
   describe('to a subscribed channel', () => {
@@ -79,6 +80,18 @@ describe('Integration: notifications', () => {
       await probot.receive({
         event: 'public',
         payload: publicEventPayload,
+      });
+    });
+
+    test('deployment_status event', async () => {
+      nock('https://slack.com').post('/api/chat.postMessage', (body) => {
+        expect(body).toMatchSnapshot();
+        return true;
+      }).reply(200, { ok: true });
+
+      await probot.receive({
+        event: 'deployment_status',
+        payload: deploymentStatusPayload,
       });
     });
   });
