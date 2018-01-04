8f53d3529561b0f98d5bf421e0618065cd09c634
Brandon Keepers <bkeepers@github.com>
2018-01-04 14:47:46 -0600

    Legacy subscriptions table

---
+++ db/migrations/20180104203617-create-legacy-subscription.js
@@ -0,0 +1,44 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('LegacySubscriptions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      workspaceSlackId: {
+        type: Sequelize.STRING,
+      },
+      repoGitHubId: {
+        type: Sequelize.BIGINT,
+      },
+      ownerGitHubId: {
+        type: Sequelize.BIGINT,
+      },
+      repoName: {
+        type: Sequelize.STRING,
+      },
+      ownerName: {
+        type: Sequelize.STRING,
+      },
+      authorSlackId: {
+        type: Sequelize.STRING,
+      },
+      channelSlackId: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('LegacySubscriptions');
+  },
+};
