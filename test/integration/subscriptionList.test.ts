9dbc522a17ac066e1b42fdc20d7f7f70807667ea
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-01-06 14:01:44 -0600

    Convert tests to typescript

--- test/integration/subscriptionList.test.ts
+++ test/integration/subscriptionList.test.ts
@@ -1,21 +1,21 @@
-const request = require('supertest');
-const nock = require('nock');
+import nock from "nock";
+import request from "supertest";
 
-const helper = require('.');
-const fixtures = require('../fixtures');
+const helper = require(".");
+const fixtures = require("../fixtures");
 
 const { probot } = helper;
 // create a bunch of subscriptions in database
 // receive slash command for subscription list
 // nock assertions
 
-describe('Integration: subscription list', () => {
+describe("Integration: subscription list", () => {
   beforeEach(async () => {
     const { SlackWorkspace, Installation, Subscription } = helper.robot.models;
 
     const workspace = await SlackWorkspace.create({
+      accessToken: "secret",
       slackId: 1,
-      accessToken: 'secret',
     });
 
     const installation = await Installation.create({
@@ -24,34 +24,34 @@ describe('Integration: subscription list', () => {
     });
 
     await Subscription.create({
+      channelId: "C2147483705",
       githubId: 1,
-      channelId: 'C2147483705',
       installationId: installation.id,
       slackWorkspaceId: workspace.id,
     });
 
     await Subscription.create({
+      channelId: "C2147483705",
       githubId: 2,
-      channelId: 'C2147483705',
       installationId: installation.id,
       slackWorkspaceId: workspace.id,
     });
   });
 
-  test('works', async () => {
-    nock('https://api.github.com').get('/repositories/1').reply(200, {
-      html_url: 'https://github.com/atom/atom',
-      full_name: 'atom/atom',
+  test("works", async () => {
+    nock("https://api.github.com").get("/repositories/1").reply(200, {
+      full_name: "atom/atom",
+      html_url: "https://github.com/atom/atom",
     });
-    nock('https://api.github.com').get('/repositories/2').reply(200, {
-      html_url: 'https://github.com/kubernetes/kubernetes',
-      full_name: 'kubernetes/kubernetes',
+    nock("https://api.github.com").get("/repositories/2").reply(200, {
+      full_name: "kubernetes/kubernetes",
+      html_url: "https://github.com/kubernetes/kubernetes",
     });
     const command = fixtures.slack.command({
-      text: 'subscribe list',
+      text: "subscribe list",
     });
 
-    await request(probot.server).post('/slack/command').send(command)
+    await request(probot.server).post("/slack/command").send(command)
      .expect(200)
      .expect((res) => {
        expect(res.body).toMatchSnapshot();
--- test/slack/renderer/native/SubscriptionList.test.ts
+++ test/slack/renderer/native/SubscriptionList.test.ts
@@ -1,42 +1,42 @@
-const { SubscriptionList } = require('../../../../lib/slack/renderer/native');
+import { SubscriptionList } from "../../../../lib/slack/renderer/native";
 
-describe('SubscriptionList', () => {
-  test('works for multiple subscriptions active in a channel', async () => {
+describe("SubscriptionList", () => {
+  test("works for multiple subscriptions active in a channel", async () => {
     const repositories = [
       {
-        html_url: 'https://github.com/bkeepers/dotenv',
-        full_name: 'bkeepers/dotenv',
+        full_name: "bkeepers/dotenv",
+        html_url: "https://github.com/bkeepers/dotenv",
       },
       {
-        html_url: 'https://github.com/atom/atom',
-        full_name: 'atom/atom',
+        full_name: "atom/atom",
+        html_url: "https://github.com/atom/atom",
       },
     ];
-    expect(new SubscriptionList(repositories, 'C01234').toJSON()).toMatchSnapshot();
+    expect(new SubscriptionList(repositories, "C01234").toJSON()).toMatchSnapshot();
   });
 
-  test('works for one subscription active in a channel', async () => {
+  test("works for one subscription active in a channel", async () => {
     const repositories = [
       {
-        html_url: 'https://github.com/bkeepers/dotenv',
-        full_name: 'bkeepers/dotenv',
+        full_name: "bkeepers/dotenv",
+        html_url: "https://github.com/bkeepers/dotenv",
       },
     ];
-    expect(new SubscriptionList(repositories, 'C01234').toJSON()).toMatchSnapshot();
+    expect(new SubscriptionList(repositories, "C01234").toJSON()).toMatchSnapshot();
   });
 
-  test('works for no subscriptions active in a channel', async () => {
+  test("works for no subscriptions active in a channel", async () => {
     const repositories = [];
-    expect(new SubscriptionList(repositories, 'C01234').toJSON()).toMatchSnapshot();
+    expect(new SubscriptionList(repositories, "C01234").toJSON()).toMatchSnapshot();
   });
 
-  test('works for one subscription active in a direct message', async () => {
+  test("works for one subscription active in a direct message", async () => {
     const repositories = [
       {
-        html_url: 'https://github.com/bkeepers/dotenv',
-        full_name: 'bkeepers/dotenv',
+        full_name: "bkeepers/dotenv",
+        html_url: "https://github.com/bkeepers/dotenv",
       },
     ];
-    expect(new SubscriptionList(repositories, 'D01234').toJSON()).toMatchSnapshot();
+    expect(new SubscriptionList(repositories, "D01234").toJSON()).toMatchSnapshot();
   });
 });
