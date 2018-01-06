44168aac59ff40baf5dc814f280a849c5b6d42a3
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-01-06 11:48:40 -0600

    Convert subscription-list to typescript

--- lib/slack/renderer/native/subscription-list.ts
+++ lib/slack/renderer/native/subscription-list.ts
@@ -1,38 +1,47 @@
-const { Message, getChannelString } = require('../');
+const { Message, getChannelString } = require("../");
+
+interface IRepository {
+  html_url: string;
+  full_name: string;
+}
+
+interface IRepositories {
+  [index: number]: IRepository;
+}
 
 module.exports = class SubscriptionList extends Message {
-  constructor(repositories, channelId) {
+  constructor(repositories: IRepositories, channelId: string) {
     super({});
     this.repositories = repositories;
     this.channel = getChannelString(channelId);
   }
 
-  repositoriesToString() {
-    return this.repositories.map(repository => (
-      `<${repository.html_url}|${repository.full_name}>`
-    ));
-  }
-
-  toJSON() {
+  public toJSON() {
     let prefix;
     if (this.channel) {
       prefix = `${this.channel}is subscribed to`;
     } else {
-      prefix = 'Subscribed to';
+      prefix = "Subscribed to";
     }
     const output = {
-      response_type: 'in_channel',
       attachments: [{
         ...this.getBaseMessage(),
-        fallback: `${prefix} ${this.repositories.length} repositor${this.repositories.length === 1 ? 'y' : 'ies'}`,
+        fallback: `${prefix} ${this.repositories.length} repositor${this.repositories.length === 1 ? "y" : "ies"}`,
       }],
+      response_type: "in_channel",
     };
     if (this.repositories.length > 0) {
       output.attachments[0].title = prefix;
-      output.attachments[0].text = this.repositoriesToString().join('\n');
+      output.attachments[0].text = this.repositoriesToString().join("\n");
       return output;
     }
     output.attachments[0].text = output.attachments[0].fallback;
     return output;
+  }
+
+  private repositoriesToString() {
+    return this.repositories.map((repository: IRepository) => (
+      `<${repository.html_url}|${repository.full_name}>`
+    ));
   }
 };
