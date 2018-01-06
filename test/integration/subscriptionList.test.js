0ddba45311b64a9461d1bea8a23088d819e957fe
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-01-06 11:06:53 -0600

    Add integration test

--- test/integration/subscriptionList.test.js
+++ test/integration/subscriptionList.test.js
@@ -9,34 +9,44 @@ const { probot } = helper;
 // receive slash command for subscription list
 // nock assertions
 
-describe('Integration: subscription list', async () => {
-  const { SlackWorkspace, Installation, Subscription } = helper.robot.models;
+describe('Integration: subscription list', () => {
+  beforeEach(async () => {
+    const { SlackWorkspace, Installation, Subscription } = helper.robot.models;
 
-  const workspace = await SlackWorkspace.create({
-    slackId: 1,
-    accessToken: 'secret',
-  });
+    const workspace = await SlackWorkspace.create({
+      slackId: 1,
+      accessToken: 'secret',
+    });
 
-  const installation = await Installation.create({
-    githubId: 1,
-    ownerId: 1,
-  });
+    const installation = await Installation.create({
+      githubId: 1,
+      ownerId: 1,
+    });
 
-  await Subscription.create({
-    githubId: 1,
-    channelId: 'C2147483705',
-    installationId: installation.id,
-    slackWorkspaceId: workspace.id,
-  });
+    await Subscription.create({
+      githubId: 1,
+      channelId: 'C2147483705',
+      installationId: installation.id,
+      slackWorkspaceId: workspace.id,
+    });
 
-  await Subscription.create({
-    githubId: 2,
-    channelId: 'C2147483705',
-    installationId: installation.id,
-    slackWorkspaceId: workspace.id,
+    await Subscription.create({
+      githubId: 2,
+      channelId: 'C2147483705',
+      installationId: installation.id,
+      slackWorkspaceId: workspace.id,
+    });
   });
 
   test('works', async () => {
+    nock('https://api.github.com').get('/repositories/1').reply(200, {
+      html_url: 'https://github.com/atom/atom',
+      full_name: 'atom/atom',
+    });
+    nock('https://api.github.com').get('/repositories/2').reply(200, {
+      html_url: 'https://github.com/kubernetes/kubernetes',
+      full_name: 'kubernetes/kubernetes',
+    });
     const command = fixtures.slack.command({
       text: 'subscribe list',
     });
