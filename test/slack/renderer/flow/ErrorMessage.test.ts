c6b4fbbc1a40e2889abbcf65e789a5e783a30c8c
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-01-08 17:38:06 +0000

    Convert ErrorMessage test to typescript

--- test/slack/renderer/flow/ErrorMessage.test.ts
+++ test/slack/renderer/flow/ErrorMessage.test.ts
@@ -1,9 +1,9 @@
-const ErrorMessage = require('../../../../lib/slack/renderer/flow/error-message');
+import ErrorMessage from "../../../../lib/slack/renderer/flow/error-message";
 
-describe('ErrorMessage', () => {
-  test('is ephemeral and has danger color', async () => {
+describe("ErrorMessage", () => {
+  test("is ephemeral and has danger color", async () => {
     const errorMessage = new ErrorMessage().getErrorMessage();
-    expect(errorMessage).toHaveProperty('response_type', 'ephemeral');
-    expect(errorMessage).toHaveProperty('attachments.0.color', 'danger');
+    expect(errorMessage).toHaveProperty("response_type", "ephemeral");
+    expect(errorMessage).toHaveProperty("attachments.0.color", "danger");
   });
 });
