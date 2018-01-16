c089ef9310a6b631a7e4bf4df8358bd02187f828
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-01-16 13:22:23 +0000

    Modify router to check if user still has acess before delivering subscription

--- lib/github/router.ts
+++ lib/github/router.ts
@@ -1,19 +1,49 @@
-const { createClient } = require('../slack/client');
+import { createClient } from "../slack/client";
+const { ReEnableSubscription } = require("../slack/renderer/flow");
+import { userHasRepoAccess } from "./helpers";
+
+// @todo: remove me and implement sequelize-typescript
+interface ISubcription {
+  githubId: number;
+  creatorId: number;
+  channelId: string;
+  SlackWorkspace: {
+    accessToken: string;
+  };
+}
 
 // Temporary "middleware" hack to look up routing before delivering event
-module.exports = ({ models }) => {
-  const { Subscription } = models;
+module.exports = ({ models }: { models: any}) => {
+  const { Subscription, SlackUser, GitHubUser } = models;
 
-  return function route(callback) {
-    return async (context) => {
+  return function route(callback: any) {
+    return async (context: any) => {
       if (context.payload.repository) {
         const subscriptions = await Subscription.lookup(context.payload.repository.id);
 
-        context.log.debug({ subscriptions }, 'Delivering to subscribed channels');
+        context.log.debug({ subscriptions }, "Delivering to subscribed channels");
 
-        return Promise.all(subscriptions.map(async (subscription) => {
+        return Promise.all(subscriptions.map(async (subscription: ISubcription) => {
           // Create clack client with workspace token
           const slack = createClient(subscription.SlackWorkspace.accessToken);
+
+          // Verify that subscription creator still has access to the resource
+          if (subscription.creatorId) {
+            const creator = await SlackUser.findById(subscription.creatorId, {
+              include: [GitHubUser],
+            });
+            const [userHasAccess, repoName] = await userHasRepoAccess(
+              subscription.githubId, creator.GitHubUser.accessToken,
+            );
+            if (!userHasAccess) {
+              // todo: disable subscription
+              await slack.chat.postMessage(
+                subscription.channelId,
+                "",
+                (new ReEnableSubscription(repoName, creator.slackId)).toJSON(),
+              );
+            }
+          }
 
           return callback(context, subscription, slack);
         }));
