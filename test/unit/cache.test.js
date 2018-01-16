10b2cd534fa1bd6372814f6badbe552b811fc5d3
Brandon Keepers <bkeepers@github.com>
2018-01-15 20:14:49 -0600

    Rename storage to cache

---
+++ test/unit/cache.test.js
@@ -0,0 +1,14 @@
+const cache = require('../../lib/cache');
+
+describe('cache', () => {
+  beforeEach(() => cache.clear());
+
+  test('sets and gets value', async () => {
+    await cache.set('foo', 'bar');
+    expect(await cache.get('foo')).toEqual('bar');
+  });
+
+  test('returns null for unset keys', async () => {
+    expect(await cache.get('nope')).toBe(undefined);
+  });
+});
