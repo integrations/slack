97609d4a2e2fe54ff03dcfe6b270a43bda5c3c2d
Brandon Keepers <bkeepers@github.com>
2018-02-15 17:38:04 -0600

    Support explicit value for settings

--- test/unit/models/subscription.test.js
+++ test/unit/models/subscription.test.js
@@ -139,6 +139,20 @@ describe('model: Subscription', () => {
       expect((await subscription.reload()).settings).toEqual({ pulls: false, issues: false });
     });
 
+    test('enables and disables sub settings', async () => {
+      subscription.enable('commits:all');
+      expect(subscription.settings).toEqual({ commits: 'all' });
+
+      subscription.disable('commits:all');
+      expect(subscription.settings).toEqual({ commits: false });
+
+      subscription.enable(['commits:all']);
+      expect(subscription.settings).toEqual({ commits: 'all' });
+
+      subscription.disable(['commits:all']);
+      expect(subscription.settings).toEqual({ commits: false });
+    });
+
     test('enables and disables with array values', () => {
       subscription.enable(['issues', 'pulls']);
       expect(subscription.settings).toEqual({ pulls: true, issues: true });
