097b51fefaca0037f1b0a146715efc9981a0508c
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-02-15 18:43:39 +0000

    Fix render test

--- test/slack/renderer/Ref.test.js
+++ test/slack/renderer/Ref.test.js
@@ -6,11 +6,8 @@ const tagCreated = require('../../fixtures/webhooks/tag_created.json');
 describe('Ref rendering', () => {
   test('works for branches', async () => {
     const refMessage = new Ref({
-      eventType: 'delete',
-      ref: branchDeleted.ref,
-      refType: branchDeleted.ref_type,
-      sender: branchDeleted.sender,
-      repository: branchDeleted.repository,
+      event: 'delete',
+      ...branchDeleted,
     });
     const rendered = refMessage.toJSON();
     expect(rendered).toMatchSnapshot();
@@ -18,11 +15,8 @@ describe('Ref rendering', () => {
 
   test('works for tags', async () => {
     const refMessage = new Ref({
-      eventType: 'create',
-      ref: tagCreated.ref,
-      refType: tagCreated.ref_type,
-      sender: tagCreated.sender,
-      repository: tagCreated.repository,
+      event: 'create',
+      ...tagCreated,
     });
     const rendered = refMessage.toJSON();
     expect(rendered).toMatchSnapshot();
