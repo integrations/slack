5fcde7e267095bdf7acf12c16bd632719b53856d
Brandon Keepers <bkeepers@github.com>
2018-02-15 15:20:33 -0600

    Fix renderers

--- test/slack/renderer/flow/Subscribed.test.js
+++ test/slack/renderer/flow/Subscribed.test.js
@@ -4,16 +4,16 @@ const repoFixture = require('../../../fixtures/repo.json');
 describe('Subscribed message rendering', () => {
   test('works for successfully subscribed', async () => {
     const message = new Subscribed({
-      channelId: 'C1234',
-      fromRepository: repoFixture,
+      subscription: { channelId: 'C1234' },
+      repository: repoFixture,
     });
     expect(message.toJSON()).toMatchSnapshot();
   });
 
   test('works for successfully unsubscribed', async () => {
     const message = new Subscribed({
-      channelId: 'C1234',
-      fromRepository: repoFixture,
+      subscription: { channelId: 'C1234' },
+      repository: repoFixture,
       unsubscribed: true,
     });
     expect(message.toJSON()).toMatchSnapshot();
@@ -21,16 +21,16 @@ describe('Subscribed message rendering', () => {
 
   test('works for direct messages', async () => {
     const message = new Subscribed({
-      channelId: 'D1234',
-      fromRepository: repoFixture,
+      subscription: { channelId: 'D1234' },
+      repository: repoFixture,
     });
     expect(message.toJSON()).toMatchSnapshot();
   });
 
   test('works for MPIM messages', async () => {
     const message = new Subscribed({
-      channelId: 'G1234',
-      fromRepository: repoFixture,
+      subscription: { channelId: 'G1234' },
+      repository: repoFixture,
     });
     expect(message.toJSON()).toMatchSnapshot();
   });
