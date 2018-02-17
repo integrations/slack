6a15dda7c08a77d2408b6f3c8185888cbc559e8d
Brandon Keepers <bkeepers@github.com>
2018-02-17 11:44:03 -0600

    Render slack message in response to slack errors

--- test/integration/error-hander.test.js
+++ test/integration/error-hander.test.js
@@ -1,8 +1,10 @@
 const request = require('supertest');
 
+const fixtures = require('../fixtures');
+
 const { probot } = require('.');
 
-describe('Integration', () => {
+describe('Error handling', () => {
   test('/boom returns 500', async () => {
     // silence logger for this test
     probot.logger.level('fatal');
@@ -13,5 +15,17 @@ describe('Integration', () => {
     // silence logger for this test
     probot.logger.level('fatal');
     await request(probot.server).get('/boom').query({ async: true }).expect(500);
+  });
+
+  test('response to failed slack commands', async () => {
+    const command = fixtures.slack.command({
+      text: 'boom',
+    });
+
+    await request(probot.server).post('/slack/command')
+      // https://api.slack.com/robots
+      .set('User-Agent', 'Slackbot 1.0 (+https://api.slack.com/robots)')
+      .send(command)
+      .expect(200, /ephemeral/);
   });
 });
