f58ffb79c5ac865998927e334621de7475d5306e
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-02-25 19:52:05 +0000

    Improve message that prompts to install GitHub App

--- test/integration/subscriptions.test.js
+++ test/integration/subscriptions.test.js
@@ -34,11 +34,12 @@ describe('Integration: subscriptions', () => {
 
   describe('authenticated user', () => {
     let slackWorkspace;
+    let user;
     beforeEach(async () => {
       const { SlackWorkspace, SlackUser, GitHubUser } = helper.robot.models;
 
       // create user
-      const user = await GitHubUser.create({
+      user = await GitHubUser.create({
         id: 2,
         accessToken: 'github-token',
       });
@@ -54,6 +55,11 @@ describe('Integration: subscriptions', () => {
     });
 
     describe('without the GitHub App installed', () => {
+      function stripUrl(message) {
+        const new_message = { ...message };
+        new_message.attachments[0].actions[0].url = 'url-is-stripped';
+        return new_message;
+      }
       test('prompts to install app', async () => {
         nock('https://api.github.com').get('/repos/atom/atom/installation').reply(404);
         nock('https://api.github.com').get('/users/atom').reply(200, fixtures.org);
@@ -82,6 +88,60 @@ describe('Integration: subscriptions', () => {
           .expect(200)
           .expect((res) => {
             expect(res.body).toMatchSnapshot();
+          });
+      });
+
+      test('user is repo owner', async () => {
+        nock('https://api.github.com').get('/repos/bkeepers/dotenv/installation').reply(404);
+        nock('https://api.github.com').get('/users/bkeepers').reply(200, {
+          type: 'User',
+          id: user.id,
+        });
+
+        const command = fixtures.slack.command({
+          text: 'subscribe bkeepers/dotenv',
+        });
+
+        await request.post('/slack/command').use(slackbot).send(command)
+          .expect(200)
+          .expect((res) => {
+            expect(stripUrl(res.body)).toMatchSnapshot();
+          });
+      });
+
+      test('owner type is other user', async () => {
+        nock('https://api.github.com').get('/repos/wilhelmklopp/wilhelmklopp/installation').reply(404);
+        nock('https://api.github.com').get('/users/wilhelmklopp').reply(200, {
+          type: 'User',
+          id: 7718702,
+        });
+
+        const command = fixtures.slack.command({
+          text: 'subscribe wilhelmklopp/wilhelmklopp',
+        });
+
+        await request.post('/slack/command').use(slackbot).send(command)
+          .expect(200)
+          .expect((res) => {
+            expect(stripUrl(res.body)).toMatchSnapshot();
+          });
+      });
+
+      test('owner type is organization', async () => {
+        nock('https://api.github.com').get('/repos/atom/atom/installation').reply(404);
+        nock('https://api.github.com').get('/users/atom').reply(200, {
+          type: 'organization',
+          id: 1089146,
+        });
+
+        const command = fixtures.slack.command({
+          text: 'subscribe atom/atom',
+        });
+
+        await request.post('/slack/command').use(slackbot).send(command)
+          .expect(200)
+          .expect((res) => {
+            expect(stripUrl(res.body)).toMatchSnapshot();
           });
       });
     });
--- test/slack/renderer/flow/InstallGitHubApp.test.js
+++ test/slack/renderer/flow/InstallGitHubApp.test.js
@@ -1,8 +1,24 @@
 const { InstallGitHubApp } = require('../../../../lib/slack/renderer/flow');
 
 describe('InstallGitHubApp message rendering', () => {
-  test('works', async () => {
-    const message = new InstallGitHubApp('https://github.com/apps/url-to-slack-app');
+  test('works without a note', async () => {
+    const message = new InstallGitHubApp({
+      installLink: 'https://github.com/apps/url-to-slack-app',
+    });
+    expect(message.toJSON()).toMatchSnapshot();
+  });
+  test('works with if owner is user ', async () => {
+    const message = new InstallGitHubApp({
+      installLink: 'https://github.com/apps/url-to-slack-app',
+      ownerType: 'User',
+    });
+    expect(message.toJSON()).toMatchSnapshot();
+  });
+  test('works if owner is organization', async () => {
+    const message = new InstallGitHubApp({
+      installLink: 'https://github.com/apps/url-to-slack-app',
+      ownerType: 'organization',
+    });
     expect(message.toJSON()).toMatchSnapshot();
   });
 });
