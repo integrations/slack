158095ef8a129e295ba14779047d000b72d87d86
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-02-26 14:58:34 +0000

    Separate display logic

--- test/messages/flow/InstallGitHubApp.test.js
+++ test/messages/flow/InstallGitHubApp.test.js
@@ -4,20 +4,38 @@ describe('InstallGitHubApp message rendering', () => {
   test('works without a note', async () => {
     const message = new InstallGitHubApp({
       installLink: 'https://github.com/apps/url-to-slack-app',
+      owner: {
+        id: 5,
+      },
+      gitHubUser: {
+        id: '5',
+      },
     });
     expect(message.toJSON()).toMatchSnapshot();
   });
   test('works with if owner is user ', async () => {
     const message = new InstallGitHubApp({
       installLink: 'https://github.com/apps/url-to-slack-app',
-      ownerType: 'User',
+      owner: {
+        id: 3,
+        type: 'User',
+      },
+      gitHubUser: {
+        id: '5',
+      },
     });
     expect(message.toJSON()).toMatchSnapshot();
   });
   test('works if owner is organization', async () => {
     const message = new InstallGitHubApp({
       installLink: 'https://github.com/apps/url-to-slack-app',
-      ownerType: 'organization',
+      owner: {
+        id: 3,
+        type: 'Organization',
+      },
+      gitHubUser: {
+        id: '5',
+      },
     });
     expect(message.toJSON()).toMatchSnapshot();
   });
