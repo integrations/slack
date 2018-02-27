c96a68b62b492d663a57e752ff303cb72a99c808
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-02-27 12:15:02 +0000

    update test accordingly

--- test/models/github-user.test.js
+++ test/models/github-user.test.js
@@ -16,9 +16,9 @@ describe('GitHubUser', () => {
       nock('https://api.github.com').get('/repositories/1').reply(200);
       await expect(user.hasRepoAccess(1)).resolves.toBe(true);
     });
-    test('throws error for unexpected status codes', async () => {
+    test('returns true for unexpected status codes', async () => {
       nock('https://api.github.com').get('/repositories/1').reply(500);
-      await expect(user.hasRepoAccess(1)).rejects.toThrow();
+      await expect(user.hasRepoAccess(1)).resolves.toBe(true);
     });
     test('returns false for user without access', async () => {
       nock('https://api.github.com').get('/repositories/1').reply(404);
