85a1017efd12eee65fa07df01268fe9caa82142c
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-04-03 15:05:00 +0100

    Extract action into seperate file

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -37,38 +37,7 @@ module.exports = {
     oauth: {
       token: require('./slack/oauth.token'),
     },
-    action: {
-      unfurl: unfurlId => ({
-        token: process.env.SLACK_VERIFICATION_TOKEN,
-        type: 'interactive_message',
-        callback_id: `unfurl-${unfurlId}`,
-        action_ts: '1522448284.018267',
-        message_ts: '1522448135.000220',
-        attachment_id: 1,
-        is_app_unfurl: false,
-        actions: [
-          {
-            name: 'unfurl',
-            type: 'button',
-            value: '',
-          },
-        ],
-        team: {
-          id: 'T000A',
-          domain: 'example',
-        },
-        channel: {
-          id: 'C74M',
-          name: 'test',
-        },
-        user: {
-          id: 'U88HS',
-          name: 'aaron',
-        },
-        response_url: 'https://hooks.slack.com/actions/1234/5678',
-        trigger_id: '13345224609.738474920.8088930838d88f008e0',
-      }),
-    },
+    action: require('./slack/action'),
   },
   github: {
     webhooks: {
