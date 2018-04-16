750036ebcbc30f2f17b75c772b91afbb1b5c3df0
Wilhelm Klopp <wilhelm.klopp@github.com>
2018-04-16 16:20:19 +0100

    Add slack metadata to support message

--- test/messages/flow/exception.test.js
+++ test/messages/flow/exception.test.js
@@ -1,10 +1,12 @@
 const Exception = require('../../../lib/messages/flow/exception');
 
 describe('Exception', () => {
-  test('renders generic error message', async () => {
-    const exception = new Exception('error-code-123').toJSON();
-    expect(exception).toHaveProperty('response_type', 'ephemeral');
-    expect(exception).toHaveProperty('attachments.0.color', 'danger');
+  test('works', async () => {
+    const exception = new Exception('error-code-123', {
+      team_id: 'T01234',
+      user_id: 'U01234',
+      channel_id: 'C01234',
+    }).toJSON();
     expect(exception).toMatchSnapshot();
   });
 });
