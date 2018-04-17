a58eafef91c509f2212e693c05d9027bca22162f
Brandon Keepers <bkeepers@github.com>
2018-04-17 08:54:09 -0500

    Use provided authorization header for GitHub requests

--- test/integration/api.test.js
+++ test/integration/api.test.js
@@ -11,49 +11,61 @@ const {
 } = models;
 
 describe('Integration: api', () => {
-  describe('authenticated user', () => {
-    let installation;
-    let workspace;
-    let githubUser;
-    let slackUser;
+  let installation;
+  let workspace;
+  let githubUser;
+  let slackUser;
 
-    beforeEach(async () => {
-      // Create an installation
-      installation = await Installation.create({
-        githubId: 1,
-        ownerId: fixtures.org.id,
-      });
-
-      // create user
-      githubUser = await GitHubUser.create({
-        id: 2,
-        accessToken: 'github-token',
-      });
-      workspace = await SlackWorkspace.create({
-        slackId: 'T0001',
-        accessToken: 'xoxp-token',
-      });
-      slackUser = await SlackUser.create({
-        slackId: 'U2147483697',
-        slackWorkspaceId: workspace.id,
-        githubId: githubUser.id,
-      });
+  beforeEach(async () => {
+    // Create an installation
+    installation = await Installation.create({
+      githubId: 1,
+      ownerId: fixtures.org.id,
     });
 
-    test('posting a message', async () => {
-      await Subscription.subscribe({
-        githubId: fixtures.repo.id,
-        channelId: 'C001',
-        slackWorkspaceId: workspace.id,
-        installationId: installation.id,
-        creatorId: slackUser.id,
-      });
+    // create user
+    githubUser = await GitHubUser.create({
+      id: 2,
+      accessToken: 'github-token',
+    });
+    workspace = await SlackWorkspace.create({
+      slackId: 'T0001',
+      accessToken: 'xoxp-token',
+    });
+    slackUser = await SlackUser.create({
+      slackId: 'U2147483697',
+      slackWorkspaceId: workspace.id,
+      githubId: githubUser.id,
+    });
 
-      nock('https://api.github.com').get('/repos/owner/repo').reply(200, fixtures.repo);
+    await Subscription.subscribe({
+      githubId: fixtures.repo.id,
+      channelId: 'C001',
+      slackWorkspaceId: workspace.id,
+      installationId: installation.id,
+      creatorId: slackUser.id,
+    });
+  });
+
+  describe('with a valid GitHub token', () => {
+    test('posting a message', async () => {
+      nock('https://api.github.com')
+        .get('/repos/owner/repo')
+        .matchHeader('authorization', 'token test')
+        .reply(200, fixtures.repo);
       nock('https://slack.com').post('/api/chat.postMessage').reply(200, { ok: true });
 
-      await request.post('/repos/owner/repo').send({ text: 'hello world' })
+      await request.post('/repos/owner/repo')
+        .set('authorization', 'token test')
+        .send({ text: 'hello world' })
         .expect(200, { ok: true });
+    });
+  });
+
+  describe('without authentication', () => {
+    test('responds with 401', async () => {
+      await request.post('/repos/owner/repo').send({ text: 'hello world' })
+        .expect(401);
     });
   });
 });
