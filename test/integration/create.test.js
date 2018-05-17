f5f2c71d5d7d48d49a4622eac89a7dfdb854fbd0
Wilhelm Klopp <wil.klopp@gmail.com>
2018-05-17 14:39:36 +0100

    Open issue from Slack by specifying repo

--- test/integration/create.test.js
+++ test/integration/create.test.js
@@ -7,15 +7,12 @@ const fixtures = require('../fixtures');
 const {
   SlackUser,
   GitHubUser,
-  Subscription,
   Installation,
 } = models;
 
 describe('Integration: Creating issues from Slack', () => {
   let workspace;
   let githubUser;
-  let slackUser;
-  let installation;
   beforeEach(async () => {
     const { SlackWorkspace } = models;
     workspace = await SlackWorkspace.create({
@@ -28,28 +25,28 @@ describe('Integration: Creating issues from Slack', () => {
       accessToken: 'secret',
     });
 
-    slackUser = await SlackUser.create({
+    await SlackUser.create({
       slackId: 'U2147483697', // same as in fixtures.slack.command
       slackWorkspaceId: workspace.id,
       githubId: githubUser.id,
     });
 
-    installation = await Installation.create({
+    await Installation.create({
       githubId: 1,
       ownerId: 1337,
     });
   });
-  test('works in channel with one subscription', async () => {
-    await Subscription.subscribe({
-      creatorId: slackUser.id,
-      slackWorkspaceId: workspace.id,
-      githubId: 54321,
-      channelId: 'C2147483705',
-      installationId: installation.id,
+  test('works when specifying a repository', async () => {
+    nock('https://api.github.com').get('/repos/kubernetes/kubernetes/installation').reply(200, {
+      id: 1337,
+      account: {
+        id: 1,
+      },
     });
 
-    nock('https://api.github.com').get('/repositories/54321').reply(200, {
+    nock('https://api.github.com').get('/repos/kubernetes/kubernetes').reply(200, {
       full_name: 'kubernetes/kubernetes',
+      id: 54321,
     });
 
     nock('https://slack.com').post('/api/dialog.open', (body) => {
@@ -58,7 +55,7 @@ describe('Integration: Creating issues from Slack', () => {
     }).reply(200, { ok: true });
 
     const command = fixtures.slack.command({
-      text: 'new issue',
+      text: 'new issue kubernetes/kubernetes',
     });
 
     await request(probot.server).post('/slack/command').send(command)
@@ -80,119 +77,6 @@ describe('Integration: Creating issues from Slack', () => {
     await request(probot.server).post('/slack/actions').send({
       payload: JSON.stringify(fixtures.slack.action.dialogSubmissionSingleRepo()),
     })
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toMatchSnapshot();
-      });
-  });
-
-
-  test('works in channel with multiple subscriptions', async () => {
-    await Subscription.subscribe({
-      creatorId: slackUser.id,
-      slackWorkspaceId: workspace.id,
-      githubId: 54321,
-      channelId: 'C2147483705',
-      installationId: installation.id,
-    });
-
-    await Subscription.subscribe({
-      creatorId: slackUser.id,
-      slackWorkspaceId: workspace.id,
-      githubId: 654321,
-      channelId: 'C2147483705',
-      installationId: installation.id,
-    });
-
-    nock('https://api.github.com').get('/repositories/54321').reply(200, {
-      full_name: 'kubernetes/kubernetes',
-    });
-
-    nock('https://api.github.com').get('/repositories/654321').reply(200, {
-      full_name: 'atom/atom',
-    });
-
-    nock('https://slack.com').post('/api/dialog.open', (body) => {
-      expect(body).toMatchSnapshot();
-      return true;
-    }).reply(200, { ok: true });
-
-    const command = fixtures.slack.command({
-      text: 'new issue',
-    });
-
-    await request(probot.server).post('/slack/command').send(command)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toMatchSnapshot();
-      });
-
-    nock('https://api.github.com').get('/repositories/54321').reply(200, {
-      name: 'kubernetes',
-      owner: {
-        login: 'kubernetes',
-      },
-    });
-
-    nock('https://api.github.com').post('/repos/kubernetes/kubernetes/issues').reply(200);
-
-    // User submits dialog to open issue
-    await request(probot.server).post('/slack/actions').send({
-      payload: JSON.stringify(fixtures.slack.action.dialogSubmissionRepoSelection()),
-    })
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toMatchSnapshot();
-      });
-  });
-
-  test('a channel with no subscriptions will show an error message', async () => {
-    const command = fixtures.slack.command({
-      text: 'new issue',
-    });
-
-    await request(probot.server).post('/slack/command').send(command)
-      .expect(200)
-      .expect((res) => {
-        expect(res.body).toMatchSnapshot();
-      });
-  });
-
-  test('repos which are not found are excluded from the list of repos to take action on', async () => {
-    await Subscription.subscribe({
-      creatorId: slackUser.id,
-      slackWorkspaceId: workspace.id,
-      githubId: 54321,
-      channelId: 'C2147483705',
-      installationId: installation.id,
-    });
-
-    await Subscription.subscribe({
-      creatorId: slackUser.id,
-      slackWorkspaceId: workspace.id,
-      githubId: 654321,
-      channelId: 'C2147483705',
-      installationId: installation.id,
-    });
-
-    nock('https://api.github.com').get('/repositories/54321').reply(404, {
-      full_name: 'kubernetes/kubernetes',
-    });
-
-    nock('https://api.github.com').get('/repositories/654321').reply(200, {
-      full_name: 'atom/atom',
-    });
-
-    nock('https://slack.com').post('/api/dialog.open', (body) => {
-      expect(body).toMatchSnapshot();
-      return true;
-    }).reply(200, { ok: true });
-
-    const command = fixtures.slack.command({
-      text: 'new issue',
-    });
-
-    await request(probot.server).post('/slack/command').send(command)
       .expect(200)
       .expect((res) => {
         expect(res.body).toMatchSnapshot();
