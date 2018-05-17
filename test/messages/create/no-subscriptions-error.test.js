c91db9726b410113d46a93e84cb5344d8d60716b
Wilhelm Klopp <wil.klopp@gmail.com>
2018-05-17 14:47:26 +0100

    Delete files that are no longer in use

--- test/messages/create/no-subscriptions-error.test.js
+++
@@ -1,8 +0,0 @@
-const NoSubscriptionsError = require('../../../lib/messages/create/no-subscriptions-error');
-
-describe('NoSubscriptionsError message rendering', () => {
-  test('works', async () => {
-    const message = new NoSubscriptionsError();
-    expect(message.getAttachment()).toMatchSnapshot();
-  });
-});
