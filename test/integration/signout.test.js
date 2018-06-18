241ec235fb4e115d06a50448e3b09633930b5f9c
Wilhelm Klopp <wil.klopp@gmail.com>
2018-06-18 18:06:51 +0100

    Delete subscriptions when user signs out

--- test/integration/signout.test.js
+++ test/integration/signout.test.js
@@ -6,12 +6,19 @@ const fixtures = require('../fixtures');
 
 const promptUrl = /^http:\/\/127\.0\.0\.1:\d+(\/github\/oauth\/login\?state=(.*))/;
 
+const {
+  SlackWorkspace,
+  GitHubUser,
+  SlackUser,
+  Installation,
+  Subscription,
+} = models;
+
 describe('Integration: signout', async () => {
   let workspace;
   let githubUser;
   let slackUser;
   beforeEach(async () => {
-    const { SlackWorkspace, GitHubUser, SlackUser } = models;
     // create workspace
     workspace = await SlackWorkspace.create({
       slackId: 'T0001',
@@ -26,7 +33,40 @@ describe('Integration: signout', async () => {
     slackUser = await SlackUser.create({
       slackId: 'U2147483697',
       slackWorkspaceId: workspace.id,
-      githubId: githubUser.id,
+    });
+
+    const installation1 = await Installation.create({
+      githubId: 1,
+      ownerId: 1,
+    });
+
+    const installation2 = await Installation.create({
+      githubId: 2,
+      ownerId: 2,
+    });
+
+    await Subscription.subscribe({
+      channelId: 'C2147483705',
+      githubId: 1,
+      installationId: installation1.id,
+      slackWorkspaceId: workspace.id,
+      creatorId: slackUser.id,
+    });
+
+    await Subscription.subscribe({
+      channelId: 'C2147483705',
+      githubId: 2,
+      installationId: installation2.id,
+      slackWorkspaceId: workspace.id,
+      creatorId: slackUser.id,
+    });
+
+    await Subscription.subscribe({
+      channelId: 'C12345',
+      githubId: 3,
+      installationId: installation2.id,
+      slackWorkspaceId: workspace.id,
+      creatorId: slackUser.id,
     });
   });
   test('a signed out user is prompted to sign in first', async () => {
@@ -66,7 +106,27 @@ describe('Integration: signout', async () => {
       );
   });
 
-  test('a signed in user is successfully signed out', async () => {
+  test('a signed in user is successfully signed out and relevant subscriptions are deleted', async () => {
+    await slackUser.update({
+      githubId: githubUser.id,
+    });
+    nock('https://api.github.com').get('/repositories/1').reply(200, {
+      full_name: 'atom/atom',
+    });
+
+    nock('https://api.github.com').get('/repositories/2').reply(200, {
+      full_name: 'kubernetes/kubernetes',
+    });
+
+    nock('https://api.github.com').get('/repositories/3').reply(200, {
+      full_name: 'integrations/slack',
+    });
+
+    nock('https://slack.com').post('/api/chat.postMessage', (body) => {
+      expect(body).toMatchSnapshot();
+      return true;
+    }).times(2).reply(200, { ok: true });
+
     const command = fixtures.slack.command({
       text: 'signout',
     });
@@ -75,5 +135,6 @@ describe('Integration: signout', async () => {
     expect(res.body.attachments[0].text).toMatchSnapshot();
 
     expect((await slackUser.reload()).githubId).toBe(null);
+    expect((await Subscription.findAll({ where: { creatorId: slackUser.id } })).length).toBe(0);
   });
 });
