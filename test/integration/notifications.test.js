226684c0db4ebbaa27f89981d6aac03233c51729
Alberto Gimeno <gimenete@github.com>
2018-09-13 15:07:43 -0700

    Initial code

--- test/integration/notifications.test.js
+++ test/integration/notifications.test.js
@@ -198,6 +198,7 @@ describe('Integration: notifications', () => {
           Accept: 'application/vnd.github.html+json',
         },
       }).get('/repos/github-slack/app/issues/31').reply(200, fixtures.issue);
+      nock('https://api.github.com').get('/repos/github-slack/app/pulls/31/reviews').reply(200);
 
       nock('https://slack.com').post('/api/chat.postMessage', (body) => {
         expect(body).toMatchSnapshot();
@@ -220,6 +221,7 @@ describe('Integration: notifications', () => {
       });
 
       nock('https://api.github.com').get(`/repositories/${pullRequestPayload.repository.id}`).reply(200);
+      nock('https://api.github.com').get('/repos/github-slack/app/pulls/31/reviews').reply(200);
 
       nock('https://slack.com').post('/api/chat.postMessage', (body) => {
         expect(body).toMatchSnapshot();
@@ -245,6 +247,7 @@ describe('Integration: notifications', () => {
       });
 
       nock('https://api.github.com').get(`/repositories/${pullRequestPayload.repository.id}`).reply(200);
+      nock('https://api.github.com').get('/repos/github-slack/app/pulls/31/reviews').reply(200);
 
       nock('https://slack.com').post('/api/chat.postMessage', (body) => {
         expect(body).toMatchSnapshot();
@@ -275,6 +278,7 @@ describe('Integration: notifications', () => {
           Accept: 'application/vnd.github.html+json',
         },
       }).get('/repos/github-slack/app/issues/31').times(2).reply(200, fixtures.issue);
+      nock('https://api.github.com').get('/repos/github-slack/app/pulls/31/reviews').reply(200);
 
       nock('https://slack.com').post('/api/chat.postMessage', (body) => {
         expect(body).toMatchSnapshot();
@@ -311,6 +315,8 @@ describe('Integration: notifications', () => {
         installationId: installation.id,
         creatorId: slackUser.id,
       });
+
+      nock('https://api.github.com').get('repos/integrations/slack/pulls/1535/reviews').reply(200);
 
       await probot.receive({
         name: 'status',
@@ -506,6 +512,7 @@ describe('Integration: notifications', () => {
           Accept: 'application/vnd.github.html+json',
         },
       }).get('/repos/github-slack/app/issues/31').reply(200, fixtures.issue);
+      nock('https://api.github.com').get('/repos/github-slack/app/pulls/31/reviews').reply(200);
 
       await probot.receive({
         name: 'pull_request',
@@ -746,8 +753,8 @@ describe('Integration: notifications', () => {
     test('does not deliver empty reviews which are actually review comments', async () => {
       const payload = reviewCommented;
       payload.review.body = null;
-
       nock('https://api.github.com').get(`/repositories/${payload.repository.id}`).reply(200);
+      nock('https://api.github.com').get(`/repos/${payload.repository.full_name}/pulls/${payload.pull_request.number}/reviews`).reply(200);
 
       await Subscription.subscribe({
         githubId: payload.repository.id,
