a68896966a89ec5bfb66545b50a814dae830c94d
Alberto Gimeno <gimenete@github.com>
2018-12-18 13:32:39 +0100

    Refactoring and added test for error case

--- test/integration/processor.test.js
+++ test/integration/processor.test.js
@@ -18,19 +18,41 @@ describe('Processor', () => {
   });
 
   test('processor is sync when NODE_ENV is "test"', async () => {
-    const syncProcessor = processor();
+    const logger = {
+      error: jest.fn(),
+    };
+    const syncProcessor = processor(logger);
 
     processed = false;
     await syncProcessor(delay());
     expect(processed).toBeTruthy();
+    expect(logger.error).not.toHaveBeenCalled();
   });
 
   test('processor is async when NODE_ENV is not "test"', async () => {
     process.env.NODE_ENV = '';
-    const asyncProcessor = processor();
+    const logger = {
+      error: jest.fn(),
+    };
+    const asyncProcessor = processor(logger);
 
     processed = false;
     await asyncProcessor(delay());
     expect(processed).toBeFalsy();
+    expect(logger.error).not.toHaveBeenCalled();
+  });
+
+  test('async error processing webhook', async () => {
+    process.env.NODE_ENV = '';
+    const logger = {
+      error: jest.fn(),
+    };
+    const asyncProcessor = processor(logger);
+
+    const err = new Error('This error is a test and is fine');
+    const rejection = Promise.reject(err);
+    await asyncProcessor(rejection);
+
+    expect(logger.error).not.toHaveBeenCalledWith([err]);
   });
 });
