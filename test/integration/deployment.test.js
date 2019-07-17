246d3072737234cd77ff98acc2cba926ec2076d0
Thomas Hughes <iamhughes@github.com>
2019-07-17 09:29:21 -0500

    Add coverage for deployment list error

--- test/integration/deployment.test.js
+++ test/integration/deployment.test.js
@@ -104,4 +104,24 @@ describe('Integration: Creating and listing deployments from Slack', () => {
         expect(res.body).toMatchSnapshot();
       });
   });
+
+  test('throws error when listing deployments and none are found', async () => {
+    nock('https://api.github.com').post('/graphql').reply(404, { errors: [{ type: 'FORBIDDEN' }] });
+    nock('https://api.github.com').get('/repos/kubernetes/kubernetes/installation')
+      .reply(200, {
+        ...fixtures.installation,
+        account: fixtures.repo.owner,
+        permissions: { },
+      });
+
+    const command = fixtures.slack.command({
+      text: 'deploy kubernetes/kubernetes list',
+    });
+
+    await request(probot.server).post('/slack/command').send(command)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toMatchSnapshot();
+      });
+  });
 });
