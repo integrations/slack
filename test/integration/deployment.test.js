e6459b31b0cbd7b89bfe742281c5a2813ad24807
Thomas Hughes <iamhughes@github.com>
2019-07-17 09:51:20 -0500

    Add create deployment test for repo w/o tags

--- test/integration/deployment.test.js
+++ test/integration/deployment.test.js
@@ -91,6 +91,61 @@ describe('Integration: Creating and listing deployments from Slack', () => {
       });
   });
 
+  test('works when specifying a repository with no tags', async () => {
+    jest.setTimeout(10000);
+    nock('https://api.github.com').get('/repos/kubernetes/kubernetes').reply(200, {
+      full_name: 'kubernetes/kubernetes',
+      id: 54321,
+    });
+    nock('https://api.github.com').get('/repos/kubernetes/kubernetes/branches').reply(200, fixtures.branches);
+    nock('https://api.github.com').get('/repos/kubernetes/kubernetes/git/refs/tags').reply(404, {});
+
+    nock('https://slack.com').post('/api/dialog.open', (body) => {
+      expect(body).toMatchSnapshot();
+      return true;
+    }).reply(200, { ok: true });
+
+    const command = fixtures.slack.command({
+      text: 'deploy kubernetes/kubernetes',
+    });
+
+    await request(probot.server).post('/slack/command').send(command)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toMatchSnapshot();
+      });
+
+    nock('https://api.github.com').get('/repositories/54321').reply(200, {
+      name: 'kubernetes',
+      owner: {
+        login: 'kubernetes',
+      },
+    });
+
+    nock('https://api.github.com').post('/repos/kubernetes/kubernetes/deployments').reply(200, {
+      ref: 'master',
+      task: 'deploy',
+      environment: 'production',
+      node_id: 'MDEwOkRlcGxveW1lbnQxMzc1ODM5ODE=',
+    });
+
+    nock('https://api.github.com').post('/graphql').reply(200, { data: { node: fixtures.deployments.data.repository.deployments.nodes[0] } });
+
+    nock('https://slack.com').post('/api/chat.postMessage', (body) => {
+      expect(body).toMatchSnapshot();
+      return true;
+    }).reply(200, { ok: true });
+
+    // User submits dialog to create a deployment
+    await request(probot.server).post('/slack/actions').send({
+      payload: JSON.stringify(fixtures.slack.action.dialogSubmissionCreateDeployment()),
+    })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toMatchSnapshot();
+      });
+  });
+
   test('works when listing deployments', async () => {
     nock('https://api.github.com').post('/graphql').reply(200, fixtures.deployments);
 
